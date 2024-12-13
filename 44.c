4343a770e4cb607d82277a6012a66cc078762bbd
Sougata2006 <psougata32@gmail.com>
2024-12-13 18:00:00 +0530

    Initial C programs (Dec 13, 2024)

--- 44.c
+++ 44.c
@@ -2,12 +2,10 @@
 
 int main (){
 int i = 8 ;
+int q = 7;
 int* j = &i;
 printf("the adress of i is %p\n",&i);
 printf("the value of i is %d\n",*j);
-
-
-
-
+printf("the value of d is %d\n",q*i);
     return 0;
 }
