6fa3da25004d2a32b74490a90507482d2a35da91
Sougata Paul <psougata32@gmail.com>
2024-12-26 22:29:03 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+printf ("give me the number\"");
+int a; // \something is a esape sequence
+scanf ("%d", &a);
+printf ("the number on your mind is %d" ,a);
+return 0;
+}
