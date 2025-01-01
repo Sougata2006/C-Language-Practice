fa0106b4ef42ad59649c38dcb3c7ea411448eb8e
Sougata Paul <psougata32@gmail.com>
2025-01-02 00:01:48 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+
+int a = 8.9 ;
+printf ("the value of a is %d\n",a);//demotion
+
+//typecasting
+
+int y;
+float m = 89.70;
+y= (int) m;
+printf("%d\n",y);
+    return 0;
+}
