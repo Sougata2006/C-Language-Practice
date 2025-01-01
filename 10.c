709e462bef54f41830b6b1e2768f0a8b626da12a
Sougata Paul <psougata32@gmail.com>
2025-01-01 23:02:30 +0530

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main (){
+// type declaration instruction
+int i = 10;
+int j = i ;
+// %d is called format specifier 
+printf ("the value of i is %d\n and value of j is %d\n",i , j);
+int a = 1,b= 2 ,c=3,d=4;
+printf ("the value of a is %d\n and value of b is %d\n",a , b);
+printf ("the value of c is %d\n and value of d is %d\n",c , d);
+
+
+// arithmetic instruction
+int x = 3 , y = 6 ;
+int z = x + y;
+printf ("the x is %d\n and the y is %d\n and the z is %d\n", x ,y,z);
+
+//modulas (%) oparetors is used to get the reminder
+printf ("the mod is %d",y%x);
+
+/* if there are any negative number in modulas then the ans will be negative
+and modulas doesnot apply on float */
+
+
+
+
+
+
+
+
+    return 0;
+}
