b94a74b97d807777c63f0fe77cc5315097e8849b
Sougata Paul <psougata32@gmail.com>
2025-01-03 23:12:10 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main (){
+
+//char only support 1 word 
+
+float a = 3.0/5 -6 ;
+printf("%f\n",a);
+
+
+int z;
+printf ("the number is ");
+scanf("%d",&z);
+printf ("gg z%97 is %d",z%97);
+
+
+
+/*  3*x/y -z+k
+6/y-z+k
+2-z+k
+-1+k
+0
+*/
+    return 0;
+}
