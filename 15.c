1c488e74ca49908dc3d3c729fc2be8b07a5ac780
Sougata Paul <psougata32@gmail.com>
2025-01-06 23:03:38 +0530

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (){
+    int a =0;
+    int b=5;
+
+
+/*
+0 false
+leftovers are true
+*/
+
+
+printf("%d\n",a&&b);
+printf ("%d\n",a||b);
+printf ("the value of not a is %d\n",!a);
+
+    return 0;
+}
