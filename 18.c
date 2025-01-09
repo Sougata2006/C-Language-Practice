6ed4e2977446d4c8b6c9f569c270f04f7f089ac6
Sougata Paul <psougata32@gmail.com>
2025-01-09 14:06:03 +0530

    Create 18.c

---
+++ 18.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+int a;
+printf ("enter a : ");
+scanf ("%d",&a);
+switch (a)
+{
+case 1 :
+printf ("you entered 1");
+    break;
+case 2 :
+printf ("you entered 2");
+    break;
+}
+    return 0 ;
+}
