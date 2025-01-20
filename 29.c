dbda396451483c70f5c08f6a7834da1e81e6bdab
Sougata Paul <psougata32@gmail.com>
2025-01-20 22:55:09 +0530

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+//function prototype
+int sum (int,int);
+
+//function defination
+int sum (int x ,int y){
+    //printf ("this sum will be %d\n" ,x+y);
+    return x+y;
+}
+int main (){
+sum(3,5);
+sum(57,56);
+sum(46,5);
+sum(6,5);
+sum(3,5);
+sum(3,5);
+sum(3,5);
+sum(3,5);
+sum(3,5);
+sum(3,5);//function call
+sum(3,5);
+int a=9,b=1;
+int c = a+b;
+printf("%d\n",c);
+
+    return 0;
+}
