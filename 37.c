3809b2184767a26d5a8902ee4847d7e2d571e2ab
Sougata Paul <psougata32@gmail.com>
2025-01-28 22:00:48 +0530

    Create 37.c

---
+++ 37.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+
+int n = 10;
+for(int i=0;i<10;i++){
+
+    for(int j =0 ;j<(2*i)+1;j++){
+
+        printf("*");
+        }
+        printf("\n");
+}
+
+
+    return 0;
+}
