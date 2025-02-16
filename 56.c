85f5cdf84345c9aa4b3206806666de0d720f4007
Sougata Paul <psougata32@gmail.com>
2025-02-16 21:14:11 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+//STRING----
+ //char string[] = {'a','b','\0'};
+
+ char string []= "abc";  // same as {char string[] = {'a','b','\0'};}
+ 
+ for(int i = 0 ; i <2;i++){
+ printf("the first chracter is %c\n",string[i]);
+ }
+
+
+
+
+
+    return 0;
+}
