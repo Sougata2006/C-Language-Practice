1b4f8cebc82b4c4e1b55e94ba50cefbcb5e7d50f
Sougata Paul <psougata32@gmail.com>
2025-03-18 23:16:11 +0530

    Create 86.c

---
+++ 86.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+// do while loop
+    int i = 0;
+    do{
+        printf("the value of i is %d\n",i);
+        i++;
+    }
+    while (i<4);
+
+
+    return 0;
+}
