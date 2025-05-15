33a5042911a0f898af06fb217217307d0ec38be7
Sougata Paul <psougata32@gmail.com>
2025-05-15 20:32:42 +0530

    Update 12.c

--- 12.c
+++ 12.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 int main (){
-
 //char only support 1 word 
-
 float a = 3.0/5 -6 ;
 printf("%f\n",a);
-
+    
+//Funny talk
 
 int z;
 printf ("the number is ");
 scanf("%d",&z);
 printf ("gg z%97 is %d",z%97);
-
-
 
 /*  3*x/y -z+k
 6/y-z+k
