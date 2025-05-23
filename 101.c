1d15cc1523655c1b3b9e4e7092486adf21e1238c
Sougata Paul <psougata32@gmail.com>
2025-05-23 10:01:04 +0530

    Update 101.c

--- 101.c
+++ 101.c
@@ -2,7 +2,7 @@
 int main()
 {
     int num;
-    //printf("Enter your number : ");
+    printf("Enter your number : ");
     if(scanf("%d",&num) != 1 || num<=0){
       printf("Invalid input. Please enter a positive integer.");
     }
